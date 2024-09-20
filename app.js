9e0c0f987644e6b8e79618f780f6c8387dabaa90
UserDevAkku <aakashverma.1234.abcd@gmail.com>
2024-09-20 23:26:08 +0530

    updated

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+
 const reqBox=document.querySelector(".requirements-box")
 const nextBtn=document.querySelector(".next")
 const chkbox=document.querySelectorAll(".chkbox")
@@ -15,172 +16,151 @@ const sdkLink=document.querySelector("#sdk-link")
 const termuxLink=document.querySelector("#termux-link")
 
 //--yesno-phone-check
-const phoneChecked=(phone)=>{
-    if(phone.checked)
-        {
-        phone.style.boxShadow="0 0 2px 2px greenyellow inset"
-        phoneYes.innerText="yes";
-        phoneYes.style.color="#013220"
-        phone.style.height="15px";
-        phone.style.width="15px";
+const phoneChecked = (phone) => {
+    if (phone.checked) {
+        phone.style.boxShadow = "0 0 2px 2px greenyellow inset";
+        phoneYes.innerText = "yes";
+        phoneYes.style.color = "#013220";
+        phone.style.height = "15px";
+        phone.style.width = "15px";
+    } else {
+        phone.style.boxShadow = "none";
+        phoneYes.innerText = "no";
+        phoneYes.style.color = "red";
     }
-    else
-    {
-        phone.style.boxShadow="none";
-        phoneYes.innerText="no";
-        phoneYes.style.color="red"
-    }
-}
-phone.addEventListener("click",()=>{
-    phoneChecked(phone,phoneYes);
-})
+};
+phone.addEventListener("click", () => {
+    phoneChecked(phone);
+});
 
 //--yesno-pc-check
-const pcChecked=(pc)=>{
-    if(pc.checked)
-        {
-        pc.style.boxShadow="0 0 2px 2px #013220 inset"
-        pcYes.innerText="yes";
-        pcYes.style.color="#013220"
-        pc.style.height="15px";
-        pc.style.width="15px";
+const pcChecked = (pc) => {
+    if (pc.checked) {
+        pc.style.boxShadow = "0 0 2px 2px greenyellow inset"; // Changed here
+        pcYes.innerText = "yes";
+        pcYes.style.color = "#013220";
+        pc.style.height = "15px";
+        pc.style.width = "15px";
+    } else {
+        pc.style.boxShadow = "none";
+        pcYes.innerText = "no";
+        pcYes.style.color = "red";
     }
-    else
-    {
-        pc.style.boxShadow="none";
-        pcYes.innerText="no";
-        pcYes.style.color="red"
-    }
-}
-pc.addEventListener("click",()=>{
-    pcChecked(pc,pcYes);
-})
-//--yesno-usb-check
-const usbChecked=(usb)=>{
-    if(usb.checked)
-        {
-        usb.style.boxShadow="0 0 2px 2px #013220 inset"
-        usbYes.innerText="yes";
-        usbYes.style.color="#013220"
-        usb.style.height="15px";
-        usb.style.width="15px";
-    }
-    else
-    {
-        usb.style.boxShadow="none";
-        usbYes.innerText="no";
-        usbYes.style.color="red"
-    }
-}
-usb.addEventListener("click",()=>{
-    usbChecked(usb,usbYes);
-})
+};
+pc.addEventListener("click", () => {
+    pcChecked(pc);
+});
 
+//--yesno-usb-check
+const usbChecked = (usb) => {
+    if (usb.checked) {
+        usb.style.boxShadow = "0 0 2px 2px greenyellow inset"; // Changed here
+        usbYes.innerText = "yes";
+        usbYes.style.color = "#013220";
+        usb.style.height = "15px";
+        usb.style.width = "15px";
+    } else {
+        usb.style.boxShadow = "none";
+        usbYes.innerText = "no";
+        usbYes.style.color = "red";
+    }
+};
+usb.addEventListener("click", () => {
+    usbChecked(usb);
+});
 
 //--yesno-sdk-check
-const sdkChecked=(sdk)=>{
-    if(sdk.checked)
-        {
-        sdk.style.boxShadow="0 0 2px 2px #013220 inset"
-       sdkYes.innerText="yes";
-        sdkYes.style.color="#013220"
-        sdkLink.style.display="none"
-        sdk.style.height="15px";
-        sdk.style.width="15px";
+const sdkChecked = (sdk) => {
+    if (sdk.checked) {
+        sdk.style.boxShadow = "0 0 2px 2px greenyellow inset"; // Changed here
+        sdkYes.innerText = "yes";
+        sdkYes.style.color = "#013220";
+        sdkLink.style.display = "none";
+        sdk.style.height = "15px";
+        sdk.style.width = "15px";
+    } else {
+        sdk.style.boxShadow = "none";
+        sdkYes.innerText = "no";
+        sdkYes.style.color = "red";
     }
-    else
-    {
-        sdk.style.boxShadow="none";
-        sdkYes.innerText="no";
-        sdkYes.style.color="red"
-        
-    }
-}
-
-sdk.addEventListener("click",()=>{
-    sdkChecked(sdk,sdkYes);
-})
+};
+sdk.addEventListener("click", () => {
+    sdkChecked(sdk);
+});
 
 //--yesno-termux-check
-const termuxChecked=(termux)=>{
-    if(termux.checked)
-        {
-        termux.style.boxShadow="0 0 2px 2px #013220 inset"
-        termuxYes.innerText="yes";
-        termuxYes.style.color="#013220"
+const termuxChecked = (termux) => {
+    if (termux.checked) {
+        termux.style.boxShadow = "0 0 2px 2px greenyellow inset"; // Changed here
+        termuxYes.innerText = "yes";
+        termuxYes.style.color = "#013220";
+    } else {
+        termux.style.boxShadow = "none";
+        termuxYes.innerText = "no";
+        termuxYes.style.color = "red";
+        termux.style.height = "15px";
+        termux.style.width = "15px";
     }
-    else
-    {
-        termux.style.boxShadow="none";
-        termuxYes.innerText="no";
-        termuxYes.style.color="red"
-        termux.style.height="15px";
-        termux.style.width="15px";
-    }
-}
-termux.addEventListener("click",()=>{
-    termuxChecked(termux,termuxYes);
-})
+};
+termux.addEventListener("click", () => {
+    termuxChecked(termux);
+});
 
 //--show-termux-link
-termux.addEventListener("mouseover",()=>{
-    if (termuxYes.innerText==="no")
-        {
-        termuxLink.style.display="display"
-        termuxLink.style.display="inline"
-        termux.disabled=true;
-   }
-},{once:true})  // {once:true} it will trigger the event handler only one time
+termux.addEventListener("mouseover", () => {
+    if (termuxYes.innerText === "no") {
+        termuxLink.style.display = "display";
+        termuxLink.style.display = "inline";
+        termux.disabled = true;
+    }
+}, { once: true });
 
 //--hide-termux-link
-termuxLink.addEventListener("click",()=>{
-   termuxLink.style.display="none"
-   termux.disabled=false;
-})
+termuxLink.addEventListener("click", () => {
+    termuxLink.style.display = "none";
+    termux.disabled = false;
+});
 
 //--show-sdk-link
-sdk.addEventListener("mouseover",()=>{
-    sdkLink.style.display="block"
-    sdkLink.style.display="inline"
-    sdk.disabled=true;
-},{once:true})// {once:true} it will trigger the event handler only one time
+sdk.addEventListener("mouseover", () => {
+    sdkLink.style.display = "block";
+    sdkLink.style.display = "inline";
+    sdk.disabled = true;
+}, { once: true });
 
 //--hide-sdk-link
-sdkLink.addEventListener("click",()=>{
-    sdkLink.style.display="none"
-    sdk.disabled=false;
- })
+sdkLink.addEventListener("click", () => {
+    sdkLink.style.display = "none";
+    sdk.disabled = false;
+});
 
- //--reqbox--css
-chkbox.forEach((chkbox)=>{
-chkbox.addEventListener("click",()=>{
-    if(phone.checked && pc.checked && usb.checked && termux.checked && sdk.checked){
-        reqBox.style.animation="paused"
-        reqBox.style.boxShadow="0 0 2px 3px green"       
-        reqBox.style.backgroundColor="#e6ffe6"
-    }
-    else{
-        reqBox.style.animation="requirements-box 999ms ease-in-out infinite both"
-        reqBox.style.backgroundColor="hsl(0, 38%, 75%)"
- 
-    }
-})
-})
+//--reqbox--css
+chkbox.forEach((chkbox) => {
+    chkbox.addEventListener("click", () => {
+        if (phone.checked && pc.checked && usb.checked && termux.checked && sdk.checked) {
+            reqBox.style.animation = "paused";
+            reqBox.style.boxShadow = "0 0 2px 3px green";
+            reqBox.style.backgroundColor = "#e6ffe6";
+        } else {
+            reqBox.style.animation = "requirements-box 999ms ease-in-out infinite both";
+            reqBox.style.backgroundColor = "hsl(0, 38%, 75%)";
+        }
+    });
+});
 
-const pl_dl=document.querySelector("#pl-dl")
-const pl_extract=document.querySelector("#pl-extract")
-const pl_copy=document.querySelector("#pl-copy")
-const pl_cdrive=document.querySelector("#pl-cdrive")
-const pl_paste=document.querySelector("#pl-paste")
-const pl_version=document.querySelector("#pl-ver")
-
+const pl_dl = document.querySelector("#pl-dl");
+const pl_extract = document.querySelector("#pl-extract");
+const pl_copy = document.querySelector("#pl-copy");
+const pl_cdrive = document.querySelector("#pl-cdrive");
+const pl_paste = document.querySelector("#pl-paste");
+const pl_version=document.querySelector("#pl-ver");
 
 // Function for pl_dl
 const dlChecked = (pl_dl) => {
     if (pl_dl.checked) {
-        pl_dl.style.boxShadow = "0 0 2px 2px #013220 inset";
-        pl_dl.style.height="15px";
-        pl_dl.style.width="15px";
+        pl_dl.style.boxShadow = "0 0 2px 2px greenyellow inset"; // Changed here
+        pl_dl.style.height = "15px";
+        pl_dl.style.width = "15px";
     } else {
         pl_dl.style.boxShadow = "none";
     }
@@ -192,9 +172,9 @@ pl_dl.addEventListener("click", () => {
 // Function for pl_extract
 const extractChecked = (pl_extract) => {
     if (pl_extract.checked) {
-        pl_extract.style.boxShadow = "0 0 2px 2px #013220 inset";
-        pl_extract.style.height="15px";
-        pl_extract.style.width="15px";
+        pl_extract.style.boxShadow = "0 0 2px 2px greenyellow inset"; // Changed here
+        pl_extract.style.height = "15px";
+        pl_extract.style.width = "15px";
     } else {
         pl_extract.style.boxShadow = "none";
     }
@@ -206,9 +186,9 @@ pl_extract.addEventListener("click", () => {
 // Function for pl_copy
 const copyChecked = (pl_copy) => {
     if (pl_copy.checked) {
-        pl_copy.style.boxShadow = "0 0 2px 2px #013220 inset";
-        pl_copy.style.height="15px";
-        pl_copy.style.width="15px";
+        pl_copy.style.boxShadow = "0 0 2px 2px greenyellow inset"; // Changed here
+        pl_copy.style.height = "15px";
+        pl_copy.style.width = "15px";
     } else {
         pl_copy.style.boxShadow = "none";
     }
@@ -220,9 +200,9 @@ pl_copy.addEventListener("click", () => {
 // Function for pl_cdrive
 const cdriveChecked = (pl_cdrive) => {
     if (pl_cdrive.checked) {
-        pl_cdrive.style.boxShadow = "0 0 2px 2px #013220 inset";
-        pl_cdrive.style.height="15px";
-        pl_cdrive.style.width="15px";
+        pl_cdrive.style.boxShadow = "0 0 2px 2px greenyellow inset"; // Changed here
+        pl_cdrive.style.height = "15px";
+        pl_cdrive.style.width = "15px";
     } else {
         pl_cdrive.style.boxShadow = "none";
     }
@@ -234,9 +214,9 @@ pl_cdrive.addEventListener("click", () => {
 // Function for pl_paste
 const pasteChecked = (pl_paste) => {
     if (pl_paste.checked) {
-        pl_paste.style.boxShadow = "0 0 2px 2px #013220 inset";
-        pl_paste.style.height="15px";
-        pl_paste.style.width="15px";
+        pl_paste.style.boxShadow = "0 0 2px 2px greenyellow inset"; // Changed here
+        pl_paste.style.height = "15px";
+        pl_paste.style.width = "15px";
     } else {
         pl_paste.style.boxShadow = "none";
     }
@@ -244,7 +224,19 @@ const pasteChecked = (pl_paste) => {
 pl_paste.addEventListener("click", () => {
     pasteChecked(pl_paste);
 });
-
+// Function for pl_version
+const versionChecked = (pl_version) => {
+    if (pl_version.checked) {
+        pl_version.style.boxShadow = "0 0 2px 2px greenyellow inset";
+        pl_version.style.height="15px";
+        pl_version.style.width="15px";
+    } else {
+        pl_version.style.boxShadow = "none";
+    }
+};
+pl_version.addEventListener("click", () => {
+    versionChecked(pl_version);
+});
 
 
 
