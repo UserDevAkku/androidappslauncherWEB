3274cb1dcb71bdee59410d3e5607a8f4701aa47e
UserDevAkku <aakashverma.1234.abcd@gmail.com>
2024-09-30 19:49:49 +0530

    updated

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
+let phone = document.querySelector("#phone");
+let pc = document.querySelector("#pc");
+let usb = document.querySelector("#usb");
+let termux = document.querySelector("#termux");
+let sdk = document.querySelector("#sdk");
+let reqBox = document.querySelector(".app-requirements-box");
+let Rstatus = document.querySelector("#R-status");
+let reqCheckbox = document.querySelectorAll(".requirements-checkboxes");
+console.log(Rstatus)
+
 // **                                       REQUIREMENTS                     **//
 // dynamically css manipulation on requirements completed by dom
 const reqCompleted = () => {
-  let phone = document.querySelector("#phone");
-  let pc = document.querySelector("#pc");
-  let usb = document.querySelector("#usb");
-  let termux = document.querySelector("#termux");
-  let sdk = document.querySelector("#sdk");
-  let reqBox = document.querySelector(".app-requirements-box");
-  let reqstatus = document.querySelector(".requirements-completed");
+ 
   if (
     phone.checked &&
     pc.checked &&
@@ -15,36 +19,58 @@ const reqCompleted = () => {
     termux.checked &&
     sdk.checked
   ) {
-    reqstatus.innerText = "requirements completed";
-    reqstatus.style.color = "red";
-    reqBox.style.boxShadow =
-      " -3px 0 5px 0px #32cd32 inset, 3px 0 5px 0px #32cd32 inset,0px -3px 5px 0px #32cd32 inset, 0 3px 5px 0px #32cd32 inset";
-    
+    Rstatus.innerHTML = "requirements completed";
+    reqBox.style.backgroundColor="#ccffcc"
+    Rstatus.style.display="block"
+    Rstatus.style.animation="Status 555ms ease-in-out infinite"
+    reqBox.style.boxShadow=" 0 0 12px 1px #32cd32";
   } else {
-    reqstatus.innerText = "";
-    reqstatus.style.color = "";
+    Rstatus.innerText = "";
     reqBox.style.boxShadow =
       " -3px 0 5px 0px #ccc inset, 3px 0 5px 0px # inset,0px -3px 5px 0px #32cd32 inset, 0 3px 5px 0px #32cd32 inset";
   }
 };
 // requirements checkboxes :- GREEN CHECKED BOX
-const reqCheckbox = document.querySelectorAll(".requirements-checkboxes");
 reqCheckbox.forEach((reqCheckbox) => {
   reqCheckbox.addEventListener("click", () => {
-    reqCheckbox.style.accentColor = "#7CFC00";
+    reqCheckbox.style.accentColor="#008000"
     reqCompleted();
   });
 });
 
 
+
 // **                                       PLATFORMTOOL                     **//
 let plBox=document.querySelector(".app-platformtool-box");
 let plCheckbox = document.querySelectorAll(".platformtool-checkboxes");
+let Pstatus=document.querySelector("#P-status")
 
+plinstalled=()=>{
+  if (
+    download.checked &&
+    extract.checked &&
+    copy.checked &&
+    cdrive.checked &&
+    paste.checked &&
+    cmd.checked && 
+    adb.checked
+  ) {
+    Pstatus.innerHTML = "platform installed";
+    plBox.style.backgroundColor="#ccffcc"
+    Pstatus.style.display="block"
+    Pstatus.style.animation="Status 555ms ease-in-out infinite"
+    plBox.style.boxShadow=" 0 0 12px 1px #32cd32";
+  } else {
+    Pstatus.innerText = "";
+    plBox.style.boxShadow =
+      " -3px 0 5px 0px #ccc inset, 3px 0 5px 0px # inset,0px -3px 5px 0px #32cd32 inset, 0 3px 5px 0px #32cd32 inset";
+  }
+};
 // platformtool checkboxes :- GREEN CHECKED BOX
 plCheckbox.forEach((plCheckbox) => {
   plCheckbox.addEventListener("click", () => {
-    plCheckbox.style.accentColor = "#7CFC00";
+    plCheckbox.style.accentColor = "#008000";
+    plinstalled();
   });
 });
 // platformtool box :- called plBoxlocked function
@@ -64,8 +90,6 @@ const plBoxlocked=()=>{
 plBox.addEventListener("click",()=>{
   plBoxlocked();
 })
-
-
 
 // androidadbconnection checkboxes :- GREEN CHECKED BOX
 const adbACheckbox = document.querySelectorAll(".adbconnection-checkboxes");
