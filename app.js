c37394c8cb8af1cd52265b0c5c79c8a255c17e56
UserDevAkku <aakashverma.1234.abcd@gmail.com>
2024-10-02 00:59:11 +0530

    updated

--- app.js
+++ app.js
@@ -16,16 +16,16 @@ const reqCompleted = () => {
     Rstatus.style.display = "block";
     Rstatus.style.animation = "Status 555ms ease-in-out infinite";
     reqBox.style.border = "2px dotted green";
+    reqCheckbox.style.accentColor = "green";
+    reqCheckbox.style.boxShadow = "0 0 0px 2px green inset";
   } else {
     Rstatus.innerText = "";
     reqBox.style.boxShadow = "0 0 12px 1px #000";
+    reqCheckbox.style.boxShadow = " 0 0 0px 0px";
   }
 };
-// requirements checkboxes :- GREEN CHECKED BOX
 reqCheckbox.forEach((reqCheckbox) => {
   reqCheckbox.addEventListener("click", () => {
-    reqCheckbox.style.accentColor = "green";
-    reqCheckbox.style.boxShadow = "0 0 0 2px green inset";
     reqCompleted();
   });
 });
@@ -35,14 +35,6 @@ let plBox = document.querySelector(".app-platformtool-box");
 let plCheckbox = document.querySelectorAll(".platformtool-checkboxes");
 let Pstatus = document.querySelector("#P-status");
 let Pverified = document.querySelector(".platformtool-verified");
-
-//**####**//
-plCheckbox.forEach((plCheckbox) => {
-  plCheckbox.addEventListener("click", () => {
-    plCheckbox.style.accentColor = "green";
-    plCheckbox.style.boxShadow = "0 0 0 2px green inset";
-  });
-});
 //**####**//
 Pverified.addEventListener("input", () => {
   if (
@@ -62,15 +54,32 @@ Pverified.addEventListener("input", () => {
     Pstatus.style.display = "block";
     Pstatus.style.animation = "Status 555ms ease-in-out infinite";
     plBox.style.border = "2px dotted green";
+    plCheckbox.style.accentColor = "green";
+    plCheckbox.style.boxShadow = "0 0 0 2px green inset";
   } else {
     Pstatus.innerHTML = "";
     Pstatus.style.display = "none";
     Pstatus.style.animation = "Status 555ms ease-in-out infinite";
     plBox.style.border = "none";
     plBox.style.backgroundColor = "#ffffff";
-
+    plCheckbox.style.accentColor = "transparent";
+    plCheckbox.style.boxShadow = "0 0 0px 0px";
   }
 });
+//**####**//
+plBox.addEventListener("mouseover",()=>{
+  if( phone.checked &&
+    pc.checked &&
+    usb.checked &&
+    termux.checked &&
+    sdk.checked){
+      plBox.style.pointerEvents="auto"
+    }else
+    {
+      plBox.style.pointerEvents="none"
+      alert("please complete all the requirements first*")
+    }
+})
 //**####**//
 
 //*                    CONFIGURATION FOR ANDROID                    *//
@@ -83,8 +92,6 @@ let Averified = document.querySelector(".Aadbconnection-verified");
 //**####**//
 ACheckbox.forEach((ACheckbox) => {
   ACheckbox.addEventListener("click", () => {
-    ACheckbox.style.accentColor = "green";
-    ACheckbox.style.boxShadow = "0 0 0 2px green inset";
   });
 });
 //**####**//
@@ -108,15 +115,43 @@ Averified.addEventListener("input", () => {
     Astatus.style.display = "block";
     Astatus.style.animation = "Status 555ms ease-in-out infinite";
     ABox.style.border = "2px dotted green";
+    ACheckbox.style.accentColor = "green";
+    ACheckbox.style.boxShadow = "0 0 0 2px green inset";
   } else {
     Astatus.innerHTML = "";
     Astatus.style.display = "none";
     Astatus.style.animation = "Status 555ms ease-in-out infinite";
     ABox.style.border = "none";
-    ABox.style.backgroundColor = "#ffffff";}
+    ABox.style.backgroundColor = "#ffffff";
+    ACheckbox.style.accentColor = "transparent";
+    ACheckbox.style.boxShadow = "0 0 0px 0px";
+  }
+
 });
 //**####**//
-
+ABox.addEventListener("mouseover",()=>{
+  if (
+    (settings.checked &&
+      miuibuild.checked &&
+      searchdeveloperoption.checked &&
+      opendeveloperoption.checked &&
+      allowforusb.checked &&
+      datacable.checked &&
+      allowforphone.checked &&
+      Acmd.checked &&
+      Aadbdevices.checked &&
+      Averified.value === "yes") ||
+    Averified.value === "YES" ||
+    Averified.value === "Yes"
+  ){
+      ABox.style.pointerEvents="auto"
+    }else
+    {
+      ABox.style.pointerEvents="none"
+      alert("please install sdk platformtool first*")
+    }
+})
+//**####**//
 
 //*                    CONFIGURATION FOR TERMUX                    *//
 let TBox = document.querySelector(".app-termuxadbconnection-box");
@@ -127,8 +162,6 @@ let Tverified = document.querySelector(".termuxadbconnection-verified");
 //**####**//
 TCheckbox.forEach((TCheckbox) => {
   TCheckbox.addEventListener("click", () => {
-    TCheckbox.style.accentColor = "green";
-    TCheckbox.style.boxShadow = "0 0 0 2px green inset";
   });
 });
 //**####**//
@@ -151,11 +184,36 @@ Tverified.addEventListener("input", () => {
    Tstatus.style.display="block"
     Tstatus.style.animation = "Status 555ms ease-in-out infinite";
     TBox.style.border = "2px dotted green";
+    TCheckbox.style.accentColor = "green";
+    TCheckbox.style.boxShadow = "0 0 0 2px green inset";
   } else {
     Tstatus.innerHTML = "";
     Tstatus.style.display = "none";
     Tstatus.style.animation = "Status 555ms ease-in-out infinite";
     TBox.style.border = "none";
-    TBox.style.backgroundColor = "#ffffff";}
+    TBox.style.backgroundColor = "#ffffff";
+    TCheckbox.style.accentColor = "transparent";
+    TCheckbox.style.boxShadow = "0 0 0px 0px";
+  }
 });
 //**####**//
+TBox.addEventListener("mouseover",()=>{
+if (VAadbdevice.checked &&
+    tcp.checked &&
+    T_install.checked &&
+    T_pkgs1.checked &&
+    T_pkgs2.checked &&
+    IP_find.checked &&
+    IP_noted.checked &&
+    IP_enter.checked &&
+    Tverified.value === "yes"||
+  Tverified.value === "YES" ||
+  Tverified.value === "Yes"
+){
+    TBox.style.pointerEvents="auto"
+  }else
+  {
+    TBox.style.pointerEvents="none"
+    alert("please complete the adb configuration for android first *")
+  }
+})
